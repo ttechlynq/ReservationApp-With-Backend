6740d3b478a374f7a5680a1bf61b3e3ec6dd5e7d
Umeifesie <chukwudi.umeifesie@accenture.com>
2023-08-18 20:42:02 +0200

    Updates

---
+++ precache-manifest.9a000275b575f042816a1e73ddf8e235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab91a9de998910377e0a",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/static/css/main.4fbfa33a.chunk.css"
+  },
+  {
+    "revision": "ab91a9de998910377e0a",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/static/js/main.67bfc055.chunk.js"
+  },
+  {
+    "revision": "a5ac18b2c60ffc7b1f33",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/static/js/runtime~main.b67048d6.js"
+  },
+  {
+    "revision": "96325be91e818d50c4c3",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/static/js/2.7d1087eb.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "25713f3ce9b1f15b3fc697ce5f6442f4",
+    "url": "/ttechlynq/ReservationApp-With-Backend.git/index.html"
+  }
+];
